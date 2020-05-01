59bbfdc7e56a614754c1a9d72ff83ef8c3305ea9
Josh Pollock <jpollock412@gmail.com>
2020-05-01 17:16:49 -0400

    Show new item on now if no item

--- src/Pages/NowPage.tsx
+++ src/Pages/NowPage.tsx
@@ -4,14 +4,25 @@ import PageTemplate from "./PageTemplate";
 import { useQuadrants } from "../SortedItems";
 import { A } from "../Theme/index";
 import { ItemThumb } from "../TodoItem/Thumbs";
-
+import NewItem from "../TodoItem/New";
+import { ItemsContext } from "../ItemsContext";
 const NowPage = () => {
 	const { getFirstItem } = useQuadrants();
 	const item = getFirstItem();
-
+	const { saveNewItem } = React.useContext(ItemsContext);
 	return (
 		<PageTemplate title={"Do This Thing Now"}>
-			<Box>{item ? <ItemThumb item={item} /> : <div>Else</div>} </Box>
+			<Box>
+				{item ? (
+					<ItemThumb item={item} />
+				) : (
+					<NewItem
+						onSave={saveNewItem}
+						titleText={"Create New Item"}
+						submitText={"Save"}
+					/>
+				)}
+			</Box>
 		</PageTemplate>
 	);
 };
